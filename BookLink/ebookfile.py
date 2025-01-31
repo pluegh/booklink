4d522079cbc847086941b541d136c5f59736f61c
Philip Lüghausen <pluegh@gmail.com>
2025-01-31 11:47:38 +0100

    Add ebook file

---
+++ BookLink/ebookfile.py
@@ -0,0 +1,21 @@
+"Class for ebook files"
+import io
+from dataclasses import dataclass
+
+from BookLink.fileregister import RegisteredFile
+from BookLink.utils import now_unixutc
+
+@dataclass
+class InMemoryEbookFile(RegisteredFile):
+    "Class for ebook files"
+    name: str
+    data: io.BytesIO
+
+    @staticmethod
+    def make(name: str, data: bytes):
+        "Create an EbookFile instance from bytes"
+        return InMemoryEbookFile(
+            name=name,
+            data=io.BytesIO(data),
+            created_at_unixutc = now_unixutc(),
+        )
---
+++ tests/test_ebookfile.py
@@ -0,0 +1,17 @@
+import pytest
+
+from BookLink.ebookfile import InMemoryEbookFile
+
+class TestInMemoryEbookFile:
+    "Test the EbookFile class"
+
+    @pytest.fixture
+    def file(self):
+        "Return a EbookFile instance"
+        yield InMemoryEbookFile.make(name="test", data=b"test")
+
+    def test_create_file(self, file):
+        "Test creating a file"
+        assert file.name == "test"
+        assert file.data.read() == b"test"
+        assert file.created_at_unixutc > 0
