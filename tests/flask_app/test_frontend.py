ef52b53d566dd2ff0c10c8e456895c386a01e80c
Philip Lüghausen <pluegh@gmail.com>
2025-03-09 10:41:07 +0100

    Move flask app into submodule

---
+++ tests/flask_app/test_frontend.py
@@ -0,0 +1,26 @@
+"""Smoke test for the frontend."""
+
+import pytest
+
+import booklink.flask_app
+
+class TestClientHandling():
+    "Test the handling of clients in pairing process"
+
+    @pytest.fixture
+    def app(self):
+        "Return the flask app"
+        app = booklink.flask_app.create_app({
+            'TESTING': True,
+            'JWT_SECRET': 'secret',
+            'MAX_CLIENTS_IN_PAIRING': 10,
+        })
+        yield app
+    
+    def test_landing_page(self, app):
+        "Test new client generation"
+        with app.test_client() as client:
+            res = client.get('/')
+            assert res.status_code == 200
+            assert res.content_type == 'text/html; charset=utf-8'
+            assert b'Welcome to BookLink' in res.data
